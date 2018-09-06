d332ca8b5bcc00794cc9d2f7d9d98f34fe5e5e6a
ClarenceR89 <clarencer89+github@gmail.com>
2018-09-06 11:19:03 +0200

    Initialize Repository

---
+++ Controllers/Home.cs
@@ -0,0 +1,23 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SpaServices.Prerendering;
+using Newtonsoft.Json;
+
+namespace react_fe_workshop.Controllers
+{
+    public class HomeController : Controller
+    {
+        [
+            Route("/"), 
+            Route("/fetchdata"), 
+            Route("/counter")
+        ]
+        public async Task<IActionResult> Index([FromServices] ISpaPrerenderer prerenderer)
+        {
+            var initialState = JsonConvert.SerializeObject(new { counter = new { count = 99 } });
+            var prerenderResult = await prerenderer.RenderToString("./ClientApp/server/bootstrap", customDataParameter: initialState);
+
+            return Content(prerenderResult.Html, "text/html");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # re_ssr_dotnet
 Dotnet core SSR with CRA
+
+# Requires:
+Dotnet Core SDK 2.1.401
+Node.js v^8.9.4
+Npm v^5.6.0
+
+# Initialize Clientapp
+```
+dotnet restore
+cd ClientApp
+npm install
+```
