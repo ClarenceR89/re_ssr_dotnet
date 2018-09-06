f685d9ccda222f7fbf8ca799f0af1c110897eb16
ClarenceR89 <clarencer89+github@gmail.com>
2018-09-06 11:27:26 +0200

    Added note

--- Controllers/Home.cs
+++ Controllers/Home.cs
@@ -7,6 +7,7 @@ namespace react_fe_workshop.Controllers
 {
     public class HomeController : Controller
     {
+        //TODO: get this info from the SEO Module
         [
             Route("/"), 
             Route("/fetchdata"), 
@@ -15,6 +16,9 @@ namespace react_fe_workshop.Controllers
         public async Task<IActionResult> Index([FromServices] ISpaPrerenderer prerenderer)
         {
             var initialState = JsonConvert.SerializeObject(new { counter = new { count = 99 } });
+
+            //Note: Pass any custom data in through here
+
             var prerenderResult = await prerenderer.RenderToString("./ClientApp/server/bootstrap", customDataParameter: initialState);
 
             return Content(prerenderResult.Html, "text/html");
